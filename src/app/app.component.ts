1d21b61f2e5ab3bf49bcfcacd050c263bcc9aa27
dmitry <dzmitry.zhytnikau@innowise-group.com>
2021-07-14 17:40:42 +0300

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'color-detect';
+  public color: string;
+  public textColor = '000000';
+  public borderValue = 40;
+
+  public test(): void {
+    const c = this.color.substring(1);
+    const rgb = parseInt(c, 16);
+    const r = (rgb >> 16) & 0xff;
+    const g = (rgb >> 8) & 0xff;
+    const b = (rgb >> 0) & 0xff;
+
+    const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    console.log(luma);
+
+    if (luma < this.borderValue) {
+      this.textColor = 'ffffff';
+    } else {
+      this.textColor = '3A3A3A';
+    }
+  }
 }
